a8bd2c9680a5f4535040b0f6f6ceb67e8ec85fb9
Pranav Bapat <7271334+pranavnbapat@users.noreply.github.com>
2024-12-13 16:39:06 +0100

    Update .gitignore to exclude .idea folder

--- test.py
+++ test.py
@@ -23,6 +23,25 @@ if response.status_code == 200:
     token = tokens.get("access")
     print(token)
 
+# # User credentials in test database
+# data = {
+#     "base_url": "http://farmcalendar:8002/",
+#     "service_name": "farm_calendar",
+#     "endpoint": "api/v1/Farm/",
+#     "methods": ["GET", "POST"],
+#     "version": "v1",
+#     "params": "name={string}&status={integer}"
+# }
+#
+# url = f"{gatekeeper_url}/api/register_service/"
+#
+# # Send POST request to get the token
+# headers = {
+#     'Authorization': f'Bearer {token}',
+# }
+#
+# response = requests.post(url, json=data, headers=headers)
+
 service_url = 'http://127.0.0.1:8001/api/proxy/weather_data/api/data/thi/?lat=12.0&lon=12.0'
 # service_url = 'http://127.0.0.1:8003/api/data/thi/?lat=12.0&lon=12.0'
 
