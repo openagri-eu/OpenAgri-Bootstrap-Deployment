ef32c15381f72032490de6c9dc8e7cd3a848bf03
Felipe Arruda Pontes <felipe.arrudapontes@maastrichtuniversity.nl>
2025-04-09 13:20:40 +0200

    removing need for editing yml files when updating deploy
    run_compose command now has this extra command for `deploy-localhost`
    that will create (ask to override if existing) a
    docker-compose.override.yml file containing the localhost port mapping
    for the services listed in the services_in_use directory.
    
    So now the yml files no longer need the localhost port mapping (these
    should be changed in the script if neeeded, or if new services are
    added).
    
    For our deployment process this would mean that after updating a service
    yml file we can just replace (or symlink) the files in services_in_use
    with the new version, and that's it.
    
    To test on a localhost setup it now requires the user to use first the
    deploy-localhost command, which will setup the override file with the
    necessary port mappings.
    refs #38

--- run_compose.py
+++ run_compose.py
@@ -1,30 +1,87 @@
 #!/usr/bin/env python
+import glob
 import os
 import subprocess
 import sys
 
 
-def create_override_file_if_not_exist(file_path):
-    if not os.path.exists(file_path):
+
+SERVICE_LOCAL_PORT_MAPPING_TEMPLATE = """
+    {service_name}:
+        ports:
+            - "${{{service_port}}}:${{{service_port}}}"
+"""
+
+SERVICE_FILE_NAMES_TO_SERVICE_NAME = {
+    'irrigationservice.yml': 'irrigation',
+    'pestmanagement.yml': 'pdm_backend',
+    'weatherservice.yml': 'weathersrv',
+}
+
+SERVICE_NAMES_TO_PORT_ENV_VARS = {
+    "gatekeeper": 'GATEKEEPER_APP_PORT',
+    "farmcalendar": 'FARM_CALENDAR_APP_PORT',
+    "pdm_backend": 'PDM_SERVICE_PORT',
+    "weathersrv": 'WEATHER_SRV_PORT',
+    "irrigation": 'IRR_SERVICE_PORT',
+    "reporting": 'REPORTING_SERVICE_PORT',
+}
+
+
+def create_override_file_if_not_exist(file_path, extra_content=None):
+
+    should_create_file = not os.path.exists(file_path)
+    if not should_create_file:
+        user_confirm = input('You are about to overwrite the existing override file. Do you want to continue? (y/[n]): ')
+        if user_confirm.lower() == 'y':
+            should_create_file = True
+
+    if should_create_file:
+        print(f"Creating override file: {file_path}")
         with open(file_path, 'w') as f:
-            f.writelines(["version: '3.9'", ''])
+            f.writelines([
+                'services:', '\n'
+            ])
+            if extra_content is not None:
+                f.write(extra_content)
+        return True
+    return False
+
+
+def deploy_localhost(override_compose_file, service_in_use_files):
+    extra_content = ""
+    for service_file_path in service_in_use_files:
+        file_name = os.path.basename(service_file_path)
+        service_name = SERVICE_FILE_NAMES_TO_SERVICE_NAME.get(file_name, file_name.split('.')[0])
+        service_port = SERVICE_NAMES_TO_PORT_ENV_VARS.get(service_name)
+        if service_port is None:
+            raise Exception(f"Warning: No port mapping found for service '{service_name}'.")
+        extra_content += SERVICE_LOCAL_PORT_MAPPING_TEMPLATE.format(service_name=service_name, service_port=service_port)
+    if create_override_file_if_not_exist(override_compose_file,  extra_content=extra_content):
+        print("Override file created successfully.")
+
+
+def handle_deploy_command(argv, override_compose_file, service_in_use_files):
+    if argv[0] == "deploy-localhost":
+        deploy_localhost(override_compose_file, service_in_use_files)
+        return True
+
+    return False
 
 
 def main():
     # Set the main docker-compose file
     main_compose_file = "docker-compose.yml"
     override_compose_file = "docker-compose.override.yml"
-    create_override_file_if_not_exist(override_compose_file)
 
     services_dir = "services_in_use"
+    service_in_use_files = glob.glob(os.path.join(services_dir, "*.yml"))
+    if handle_deploy_command(sys.argv[1:], override_compose_file, service_in_use_files):
+        # exists if used our deploy-localhost command
+        # we don't want to pass this to docker compose
+        return
 
-    compose_files = [main_compose_file]
-
-    for service_file in os.listdir(services_dir):
-        if service_file.endswith(".yml"):
-            compose_files.append(os.path.join(services_dir, service_file))
-
-    compose_files.append(override_compose_file)
+    compose_files = [main_compose_file] + service_in_use_files + [override_compose_file]
 
     compose_command = ["docker", "compose"]
     for compose_file in compose_files:
